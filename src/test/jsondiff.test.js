c48ea1a9e363cce3e2088eb75f5a5ffbc8378ee0
Andy Gayton <andy@thecablelounge.com>
2011-06-16 15:28:26 +1000

    jsondiffs object comparison > js

--- src/test/jsondiff.test.js
+++ src/test/jsondiff.test.js
@@ -1,6 +1,6 @@
 var jd = new jsondiff();
 test('listdiff', function() {  
-    console.log(jd.diff([1], [1,4]));
-    console.log({'o': 'L', 'v': {1: {'o':'+', 'v':4}}});
-    equals(jd.diff([1], [1,4]), {'o': 'L', 'v': {1: {'o':'+', 'v':4}}}, 'append list item')
+    var got = jd.diff([1], [1,4]);
+    var want = {'o': 'L', 'v': {1: {'o':'+', 'v':4}}};
+    ok(jd.equals(want, got), 'append list item');
 });
